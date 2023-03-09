ad5238c741420b8c4a928e89a7dd6264b6b0a59c
taipt204 <taiptpd08109@fpt.edu.vn>
2023-03-09 04:51:22 -0800

    init

---
+++ lab1/app.js
@@ -0,0 +1,12 @@
+// nhập số a từ bàn phím.
+let a = prompt("Nhập số nguyên a:");
+
+// nhập b từ bàn phím.
+let b =prompt("Nhập số nguyên b:");
+
+let c = parseInt(a)+parseInt(b);
+//in ra màn hình.
+
+document.write(c);
+// show giá trị biến.
+console.log(c);
